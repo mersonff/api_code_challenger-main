580bb958671063d67e7ecdafbce46e5629d078fc
Emerson Freitas <mersonsi@gmail.com>
2024-01-07 15:53:30 -0300

    feat: create action

--- app/controllers/api/v1/dns_records_controller.rb
+++ app/controllers/api/v1/dns_records_controller.rb
@@ -8,7 +8,19 @@ module Api
 
       # POST /dns_records
       def create
-        # TODO: Implement this action
+        dns_record = DnsRecord.new(dns_record_params)
+
+        if dns_record.save
+          render json: { id: dns_record.id }, status: :created
+        else
+          render json: dns_record.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def dns_record_params
+        params.require(:dns_records).permit(:ip, hostnames_attributes: [:hostname])
       end
     end
   end
